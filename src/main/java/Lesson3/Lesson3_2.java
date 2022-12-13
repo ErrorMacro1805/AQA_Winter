3a4f84ed17e1dc02e62d86c64ef8e387b4b84c63
KhvanEvgeniy <khvanevgeniy05@gmail.com>
2022-12-13 11:46:26 +0200

    Corrections

---
+++ src/main/java/Lesson3/Lesson3_2.java
@@ -0,0 +1,21 @@
+package Lesson3;
+
+public class Lesson3_2 {
+    public static void main(String[] args) {
+
+
+     //   int y;
+      //  y = 10;
+       // int[] array;
+       // array = new int[20];
+       // array[0] = 12;
+      //  array[1] = 13;
+       // System.out.println(array[2]);
+int [] array =  {1, 19, 33, 4, 56, 76, 14, 36, 36, 78,987};
+for (int i =0; i<array.length; i++){
+    if (array[i]==14){
+        System.out.println("index = "+i);
+    }
+}
+    }
+}
