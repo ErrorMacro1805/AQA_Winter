9dcbdc4be31f3002537ebdf5ab27fbb58d8c9af1
KhvanEvgeniy <khvanevgeniy05@gmail.com>
2023-01-05 12:56:25 +0200

    Lesson5

---
+++ src/main/java/Lesson5/Main.java
@@ -0,0 +1,13 @@
+package Lesson5;
+
+public class Main {
+    public static void main(String[] args) {
+       // char[] str1= {'a','b','c'};
+        String str="hello";
+        String k = "helo";
+        boolean eq = str ==k;
+     //  String helo= str.concat(" Hello");
+        System.out.println(eq);
+    }
+}
+
